085168ab5aa616eeb96a9535744bf95bc75569fa
sptheme <sopheak.peas@gmail.com>
2015-02-11 14:41:12 +0700

    Theme setup

---
+++ library/functions/meta-boxes.php
@@ -0,0 +1,196 @@
+<?php
+
+/*  Initialize the meta boxes.
+/* ------------------------------------ */
+add_action( 'admin_init', '_custom_meta_boxes' );
+
+function _custom_meta_boxes() {
+
+	$prefix = 'sp_';
+  
+/*  Custom meta boxes
+/* ------------------------------------ */
+$page_layout_options = array(
+	'id'          => 'page-options',
+	'title'       => 'Page Options',
+	'desc'        => '',
+	'pages'       => array( 'page', 'post' ),
+	'context'     => 'normal',
+	'priority'    => 'default',
+	'fields'      => array(
+		array(
+			'label'		=> 'Primary Sidebar',
+			'id'		=> $prefix . 'sidebar_primary',
+			'type'		=> 'sidebar-select',
+			'desc'		=> 'Overrides default'
+		),
+		array(
+			'label'		=> 'Layout',
+			'id'		=> $prefix . 'layout',
+			'type'		=> 'radio-image',
+			'desc'		=> 'Overrides the default layout option',
+			'std'		=> 'inherit',
+			'choices'	=> array(
+				array(
+					'value'		=> 'inherit',
+					'label'		=> 'Inherit Layout',
+					'src'		=> SP_ASSETS . '/images/admin/layout-off.png'
+				),
+				array(
+					'value'		=> 'col-1c',
+					'label'		=> '1 Column',
+					'src'		=> SP_ASSETS . '/images/admin/col-1c.png'
+				),
+				array(
+					'value'		=> 'col-2cl',
+					'label'		=> '2 Column Left',
+					'src'		=> SP_ASSETS . '/images/admin/col-2cl.png'
+				),
+				array(
+					'value'		=> 'col-2cr',
+					'label'		=> '2 Column Right',
+					'src'		=> SP_ASSETS . '/images/admin/col-2cr.png'
+				)
+			)
+		)
+	)
+);
+
+/* ---------------------------------------------------------------------- */
+/*	Post Format: video
+/* ---------------------------------------------------------------------- */
+$post_format_video = array(
+	'id'          => 'format-video',
+	'title'       => 'Format: Video',
+	'desc'        => 'These settings enable you to embed videos into your posts.',
+	'pages'       => array( 'post' ),
+	'context'     => 'normal',
+	'priority'    => 'high',
+	'fields'      => array(
+		array(
+			'label'		=> 'Video URL',
+			'id'		=> $prefix . 'video_url',
+			'type'		=> 'text',
+			'desc'		=> 'Enter Video Embed URL from youtube, vimeo or dailymotion'
+		)
+	)
+);
+
+/* ---------------------------------------------------------------------- */
+/*	Post Format: Audio
+/* ---------------------------------------------------------------------- */
+$post_format_audio = array(
+	'id'          => 'format-audio',
+	'title'       => 'Format: Audio',
+	'desc'        => 'These settings enable you to embed audio into your posts. You must provide both .mp3 and .ogg/.oga file formats in order for self hosted audio to function accross all browsers.',
+	'pages'       => array( 'post' ),
+	'context'     => 'normal',
+	'priority'    => 'high',
+	'fields'      => array(
+		array(
+			'label'		=> 'Audio URL',
+			'id'		=> $prefix . 'audio_url',
+			'type'		=> 'upload',
+			'desc'		=> 'You can get sound or audio URL from soundcloud'
+		)
+	)
+);
+
+/* ---------------------------------------------------------------------- */
+/*	Post Format: Gallery
+/* ---------------------------------------------------------------------- */
+$post_format_gallery = array(
+	'id'          => 'format-gallery',
+	'title'       => 'Format: Gallery',
+	'desc'        => 'Standard post galleries.</i>',
+	'pages'       => array( 'post' ),
+	'context'     => 'normal',
+	'priority'    => 'high',
+	'fields'      => array(
+		array(
+			'label'		=> 'Upload photo',
+			'id'		=> $prefix . 'post_gallery',
+			'type'		=> 'gallery',
+			'desc'		=> 'Upload photos'
+		)
+	)
+);
+
+/* ---------------------------------------------------------------------- */
+/*	Post Format: Chat
+/* ---------------------------------------------------------------------- */
+$post_format_chat = array(
+	'id'          => 'format-chat',
+	'title'       => 'Format: Chat',
+	'desc'        => 'Input chat dialogue.',
+	'pages'       => array( 'post' ),
+	'context'     => 'normal',
+	'priority'    => 'high',
+	'fields'      => array(
+		array(
+			'label'		=> 'Chat Text',
+			'id'		=> $prefix . 'chat',
+			'type'		=> 'textarea',
+			'rows'		=> '2'
+		)
+	)
+);
+/* ---------------------------------------------------------------------- */
+/*	Post Format: Link
+/* ---------------------------------------------------------------------- */
+$post_format_link = array(
+	'id'          => 'format-link',
+	'title'       => 'Format: Link',
+	'desc'        => 'Input your link.',
+	'pages'       => array( 'post' ),
+	'context'     => 'normal',
+	'priority'    => 'high',
+	'fields'      => array(
+		array(
+			'label'		=> 'Link Title',
+			'id'		=> $prefix . 'link_title',
+			'type'		=> 'text'
+		),
+		array(
+			'label'		=> 'Link URL',
+			'id'		=> $prefix . 'link_url',
+			'type'		=> 'text'
+		)
+	)
+);
+
+/* ---------------------------------------------------------------------- */
+/*	Post Format: quote
+/* ---------------------------------------------------------------------- */
+$post_format_quote = array(
+	'id'          => 'format-quote',
+	'title'       => 'Format: Quote',
+	'desc'        => 'Input your quote.',
+	'pages'       => array( 'post' ),
+	'context'     => 'normal',
+	'priority'    => 'high',
+	'fields'      => array(
+		array(
+			'label'		=> 'Quote',
+			'id'		=> $prefix . 'quote',
+			'type'		=> 'textarea',
+			'rows'		=> '2'
+		),
+		array(
+			'label'		=> 'Quote Author',
+			'id'		=> $prefix . 'quote_author',
+			'type'		=> 'text'
+		)
+	)
+);
+
+/*  Register meta boxes
+/* ------------------------------------ */
+	ot_register_meta_box( $page_layout_options );
+	ot_register_meta_box( $post_format_audio );
+	ot_register_meta_box( $post_format_chat );
+	ot_register_meta_box( $post_format_gallery );
+	ot_register_meta_box( $post_format_link );
+	ot_register_meta_box( $post_format_quote );
+	ot_register_meta_box( $post_format_video );
+}
